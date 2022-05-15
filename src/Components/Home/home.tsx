6da1b1f989e341ba892f2f3f3a373620a737d7b0
Abiodun Ganiyu <timothyabiodun300@gmail.com>
2022-05-15 01:06:27 +0100

    work in experience

--- src/Components/Home/home.tsx
+++ src/Components/Home/home.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../Header/header";
 import NavBar from "../Navbar/Navbar";
 import styles from './homepage.module.scss';
+import location from "../../assets/images/location.svg";
 
 const Home = () => {
     return (
@@ -11,9 +12,34 @@ const Home = () => {
                 <Header />
                 <div className={styles['floating-container']}>
                     <div className={styles['feature-container']}>
-                        
+                        <div className={styles['feature']}>
+                            <img src={location} alt="location icon"></img>
+                            <span>Destination</span>
+                            <select name="places" id="places">
+                                <option value="Kigali">Kigali, Riwanda</option>
+                                <option value="South Africa">South Africa</option>
+                            </select>
+                        </div>
+                        <div className={styles['feature']}>
+                            <img src={location} alt="location icon"></img>
+                            <span>Destination</span>
+                            <select name="places" id="places">
+                                <option value="Kigali">Kigali, Riwanda</option>
+                                <option value="South Africa">South Africa</option>
+                            </select>
+                        </div>
+                        <div className={styles['feature']}>
+                            <img src={location} alt="location icon"></img>
+                            <span>Destination</span>
+                            <select name="places" id="places">
+                                <option value="Kigali">Kigali, Riwanda</option>
+                                <option value="South Africa">South Africa</option>
+                            </select>
+                        </div>
+                        <div className={styles['search']}>Search</div>
                     </div>
                 </div>
+                <div className={styles['new-age']}>Another section</div>
             </div>
             
         </div>
