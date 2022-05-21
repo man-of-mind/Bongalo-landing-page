93d7ba569b880e25efe40902db5472f2b7cb4d32
Abiodun Ganiyu <timothyabiodun300@gmail.com>
2022-05-21 22:28:38 +0100

    completed desktop view

--- src/Components/Home/home.tsx
+++ src/Components/Home/home.tsx
@@ -31,6 +31,13 @@ import digitalOcean from "../../assets/images/digitalOcean.svg";
 import sendGrid from "../../assets/images/sendGrid.svg";
 import nkap from "../../assets/images/nkap.svg";
 import logo from "../../assets/images/bangolo_logo.svg";
+import email from "../../assets/images/email.svg";
+import youtube from "../../assets/images/youtube.svg";
+import facebook from "../../assets/images/facebook.svg";
+import twitter from "../../assets/images/twitter.svg";
+import instagram from "../../assets/images/instagram.svg";
+import pay from "../../assets/images/pay.svg";
+import visa from "../../assets/images/visa.svg";
 
 const Home = () => {
     return (
@@ -54,7 +61,7 @@ const Home = () => {
                             </div>
                             <span>Arrival - Departure</span>
                             <select name="places" id="places">
-                                <option value="Kigali">Kigali, Riwanda</option>
+                                <option value="week 10">Wed, Oct 1 - Tue, Dec 10</option>
                                 <option value="South Africa">South Africa</option>
                             </select>
                         </div>
@@ -64,8 +71,8 @@ const Home = () => {
                             </div>
                             <span>Guest</span>
                             <select name="places" id="places">
-                                <option value="Kigali">Kigali, Riwanda</option>
-                                <option value="South Africa">South Africa</option>
+                                <option value="5">5 Guests</option>
+                                <option value="10">10 Guests</option>
                             </select>
                         </div>
                         <div className={styles['search']}>Search</div>
@@ -268,7 +275,73 @@ const Home = () => {
                     <div className={styles['footer-logo-section']}>
                         <img src={logo} alt="bangolo logo"></img>
                         <span>Bongalo</span>
-                        <div></div>
+                        <div className={styles['newsletter']}>
+                            <div className={styles['text']}>Enter your email to subscribe to our newsletter</div>
+                            <div className={styles['input-button']}>
+                                <div className={styles['input-container']}>
+                                    <img src={email} alt="email logo"></img>
+                                    <input
+                                        type="text"
+                                        placeholder="Enter your email address"></input>
+                                </div>
+                                <button>Subscribe</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles['footer-table']}>
+                        <table>
+                            <tr>
+                                <th>ABOUT BONGALO</th>
+                                <th>LINKS</th>
+                                <th>CONTACT</th>
+                            </tr>
+                            <tr>
+                                <td>About us</td>
+                                <td>Blog</td>
+                                <td>4th Floor, Fairview Building</td>
+                            </tr>
+                            <tr>
+                                <td>Company</td>
+                                <td>Privacy Policy</td>
+                                <td>KG 622 St, Kigali, Rwanda</td>
+                            </tr>
+                            <tr>
+                                <td>Career</td>
+                                <td>Terms &#38; Conditions</td>
+                            </tr>
+                            <tr>
+                                <td>Press Release</td>
+                                <td></td>
+                                <td>6th Floor CamCull Building</td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td>Comm Ave, Bamenda, Cameroon</td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td>info@bongalo.co</td>
+                            </tr>
+                        </table>
+                        <div className={styles['join-us']}>
+                            <div>JOIN US</div>
+                            <div className={styles['social-media-icons']}>
+                                <img src={youtube} alt="youtube icon"></img>
+                                <img src={twitter} alt="twitter icon"></img>
+                                <img src={facebook} alt="facebbok icon"></img>
+                                <img src={instagram} alt="instagram icon" className={styles['ig']}></img>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles['copyright-section']}>
+                        <span>Copyright &#169; 2021 Bongal.co. All rights reserved</span>
+                        <div className={styles['payment-option']}>
+                            <span>PAYMENT METHODS</span>
+                            <img src={pay} alt="visa logo"></img>
+                            <img src={visa} alt="visa"></img>
+                        </div>
                     </div>
                 </footer>
             </div>
