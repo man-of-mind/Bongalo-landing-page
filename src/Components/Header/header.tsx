32eff03c208159ea5ee1951e347c81fbc33b6af6
Abiodun Ganiyu <timothyabiodun300@gmail.com>
2022-05-21 23:57:28 +0100

    created footer component

--- src/Components/Header/header.tsx
+++ src/Components/Header/header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from './header.module.scss';
 import Previous from '../../assets/images/previousIcon.svg';
 import Next from '../../assets/images/nextIcon.svg';
-import DownIcon from '../../assets/images/downIcon.svg';
+import downIcon from "../../assets/images/downIcon.svg";
 
 
 const Header = () => {
@@ -15,7 +15,9 @@ const Header = () => {
                 </div>
                 <img src={Next} alt="view next text"></img>
             </div>
-            <img src={Next} alt="view next text" className={styles['down-pic']}></img>
+            <div className={styles['circle']}>
+                <img src={downIcon} alt="down"></img>
+            </div>
         </div>
     );
 }
--- src/Components/Home/home.tsx
+++ src/Components/Home/home.tsx
@@ -30,14 +30,8 @@ import googleCloud from "../../assets/images/googleCloud.svg";
 import digitalOcean from "../../assets/images/digitalOcean.svg";
 import sendGrid from "../../assets/images/sendGrid.svg";
 import nkap from "../../assets/images/nkap.svg";
-import logo from "../../assets/images/bangolo_logo.svg";
-import email from "../../assets/images/email.svg";
-import youtube from "../../assets/images/youtube.svg";
-import facebook from "../../assets/images/facebook.svg";
-import twitter from "../../assets/images/twitter.svg";
-import instagram from "../../assets/images/instagram.svg";
-import pay from "../../assets/images/pay.svg";
-import visa from "../../assets/images/visa.svg";
+import Footer from "../Footer/footer";
+
 
 const Home = () => {
     return (
@@ -271,81 +265,9 @@ const Home = () => {
                     <img src={googleCloud} alt="google cloud logo"></img>
                     <img src={nkap} alt="nkap logo"></img>
                 </div>
-                <footer>
-                    <div className={styles['footer-logo-section']}>
-                        <img src={logo} alt="bangolo logo"></img>
-                        <span>Bongalo</span>
-                        <div className={styles['newsletter']}>
-                            <div className={styles['text']}>Enter your email to subscribe to our newsletter</div>
-                            <div className={styles['input-button']}>
-                                <div className={styles['input-container']}>
-                                    <img src={email} alt="email logo"></img>
-                                    <input
-                                        type="text"
-                                        placeholder="Enter your email address"></input>
-                                </div>
-                                <button>Subscribe</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={styles['footer-table']}>
-                        <table>
-                            <tr>
-                                <th>ABOUT BONGALO</th>
-                                <th>LINKS</th>
-                                <th>CONTACT</th>
-                            </tr>
-                            <tr>
-                                <td>About us</td>
-                                <td>Blog</td>
-                                <td>4th Floor, Fairview Building</td>
-                            </tr>
-                            <tr>
-                                <td>Company</td>
-                                <td>Privacy Policy</td>
-                                <td>KG 622 St, Kigali, Rwanda</td>
-                            </tr>
-                            <tr>
-                                <td>Career</td>
-                                <td>Terms &#38; Conditions</td>
-                            </tr>
-                            <tr>
-                                <td>Press Release</td>
-                                <td></td>
-                                <td>6th Floor CamCull Building</td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td>Comm Ave, Bamenda, Cameroon</td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td>info@bongalo.co</td>
-                            </tr>
-                        </table>
-                        <div className={styles['join-us']}>
-                            <div>JOIN US</div>
-                            <div className={styles['social-media-icons']}>
-                                <img src={youtube} alt="youtube icon"></img>
-                                <img src={twitter} alt="twitter icon"></img>
-                                <img src={facebook} alt="facebbok icon"></img>
-                                <img src={instagram} alt="instagram icon" className={styles['ig']}></img>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={styles['copyright-section']}>
-                        <span>Copyright &#169; 2021 Bongal.co. All rights reserved</span>
-                        <div className={styles['payment-option']}>
-                            <span>PAYMENT METHODS</span>
-                            <img src={pay} alt="visa logo"></img>
-                            <img src={visa} alt="visa"></img>
-                        </div>
-                    </div>
-                </footer>
+                
+                <Footer />
             </div>
-            
         </div>
     );
 }
